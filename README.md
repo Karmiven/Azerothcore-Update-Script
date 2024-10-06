e254022e0e08b8fde147b40b3da072ef753f5248
David Wlodarczyk <anyeight@gmail.com>
2024-10-06 11:50:04 -0500

    Added optional parameters

--- README.md
+++ README.md
@@ -6,14 +6,20 @@ Copy the script to a location of your choosing.  Edit the file and update the IN
 
 ```bash
 git clone https://github.com/grndal/Azerothcore-Update-Script
-chmod +x update.sh
+chmod u+x updatemods.sh
+```
+
+##Options
+```
+-d : deletes the build directory as defined by the variable BUILD_DIR.  This is an optional parameter.
+-c [build | compile] : After the update, will compile Azerothcore with either the BUILD or COMPILE option.  This is an optional parameter.
 ```
 
 ##Running the Script
-To run the script, simply navigate to where it was installed and type
+To run the script, simply navigate to where it was installed and type with any options
 
 ```bash
-./update.sh
+./updatemods.sh
 ```
 
 Optionally, you can update your environment settings to include an alias so that it can be executed anywhere.
--- updatemods.sh
+++ updatemods.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #Setup some variables to handle colored text
 CYAN='\033[0;36m'
@@ -7,23 +7,73 @@ NC='\033[0m'
 
 #This is the installation directory of your azerothcore.  Do NOT use a tilda ~ when referencing the installation directory; this will cause the script to fail
 INSTALL_DIR="/root/azerothcore-wotlk"
+BUILD_DIR="/var/build"
+COMPILE_TYPE=
+
+#Variables for command-line options.  Update as needed.
+COMPILE_CMD='./acore.sh compiler all'
+BUILD_CMD='./acore.sh compiler build'
 
 cd $INSTALL_DIR
 
-echo -e "${YELLOW}Updating Azerothcore${NC}"
-git pull
+#Prompt user for correct usage of update script
+usage() 
+	{
+		echo "Usage ./updatemods.sh [-d] [-c build | compile]";
+		exit 0
+	}
 
-cd $INSTALL_DIR/modules
+
+#Update function for AzerothCore and all modules
+update_az()
+        {
+                echo -e "${YELLOW}Updating Azerothcore${NC}"
+                git pull
+
+                cd $INSTALL_DIR/modules
+
+                for f in *; do
+                    if [ -d "$f" ]; then
+                        # Will not run if no directories are available
+                        echo -e "Updating mod ${CYAN}$f...${NC}"
+                       git pull
+                    fi
+                done
+        }
 
 
-for f in *; do
-    if [ -d "$f" ]; then
-        # Will not run if no directories are available
-        echo -e "Updating mod ${CYAN}$f...${NC}"
-	git pull
-    fi
+#Call the update function
+update_az
+
+while getopts "hdc:" opt; do
+	case $opt in
+		d)
+			echo -e "${YELLOW}Deleting build directory${NC}"
+			rm -rf $INSTALL_DIR$BUILD_DIR
+			;;
+		c)
+			COMPILE_TYPE=${OPTARG,,}
+			case $COMPILE_TYPE in
+				compile)
+					echo -e "${YELLOW}Compiling with the all option${NC}"
+					cd $INSTALL_DIR
+					$COMPILE_CMD
+					;;
+				build)
+					echo -e "${YELLOW}Compiling with the build option${NC}"
+					cd $INSTALL_DIR
+					$BUILD_CMD
+					;;
+				*)
+					usage
+					;;
+			esac
+			;;
+		h | *)
+			usage
+			;;
+	esac
 done
-
-
+shift $((OPTIND-1))
 
 
